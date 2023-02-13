01525137055727cfa02807d88674ee9d044f68fb
Sandra Reinecke <sandra.reinecke@consid.se>
2023-02-13 09:20:04 +0100

    generated spring boot project

---
+++ dog-info/src/main/java/com/animal/doginfo/DogInfoApplication.java
@@ -0,0 +1,13 @@
+package com.animal.doginfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogInfoApplication.class, args);
+	}
+
+}
---
+++ dog-info/src/test/java/com/animal/doginfo/DogInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animal.doginfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
