9a5feeb739bc8991dea4580a613ccc1d505b079c
Sandra Reinecke <sandra.reinecke@consid.se>
2023-02-22 11:31:00 +0100

    .jar

--- src/main/java/com/animal/doginfo/DogInfoApplication.java
+++ src/main/java/com/animal/doginfo/DogInfoApplication.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DogInfoApplication {
