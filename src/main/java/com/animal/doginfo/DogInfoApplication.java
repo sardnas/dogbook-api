530726820bd832175cc3cb8c17a600f460f0da7f
Sandra Reinecke <sandra.reinecke@consid.se>
2023-02-22 12:41:11 +0100

    removed some unused imports

--- src/main/java/com/animal/doginfo/DogInfoApplication.java
+++ src/main/java/com/animal/doginfo/DogInfoApplication.java
@@ -2,12 +2,7 @@ package com.animal.doginfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.SpringApplication;
+
 
 @SpringBootApplication
 public class DogInfoApplication {
